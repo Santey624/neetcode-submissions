23af5a46b989ed4e8f761df2674a6151d1f15dc3
santoshgairesharma <92208766+Santey624@users.noreply.github.com>
2026-08-11 23:55:09 +0900

    Update submission-4.py

--- Data Structures & Algorithms/two-integer-sum/submission-4.py
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -6,6 +6,6 @@ class Solution:
 
             if needed in value:
                 return[value[needed],i]
-            value[nums[i]] = i
+            value[nums[i]] = i #This actually stores the key-> value pair so we have to do this. For first element it stores: [0,3] where 0 is the index and 3 is the value
 
         return []
